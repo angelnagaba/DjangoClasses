ebe5552e412816c6461d2f5a12d88e2fb6a18024
angelnagaba <angelnagaba99@gmail.com>
2020-06-24 13:21:10 +0300

    Learnt forms

--- blog/forms.py
+++ blog/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
+    #title = forms.CharField(label = "email", widget = forms.TextInput(attrs={'class':'form-control'}),required = True)
 
     class Meta:
         model = Post
